814f34349c225945493cabee91694339a88e7ee2
DenisErmolovich <denisermolovich@fico.com>
2019-07-03 14:39:09 +0300

    progress

--- by/oca/appendixb/CompileFloat.java
+++ by/oca/appendixb/CompileFloat.java
@@ -2,6 +2,10 @@ package by.oca.appendixb;
 
 public class CompileFloat {
 	public static void main(String... args) {
-		System.out.println("Hello world!");
+		float f1 = 102;
+		float f2 = (int) 102.0;
+		float f3 = 1f * 0.0; //possible lossy conversion from double to float
+		float f4 = 1f * (short)0.0;
+		float f5 = 1f * (boolean)0; //int can't be converted to boolean
 	}
 }
