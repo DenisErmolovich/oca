bdc3fa71dc9cbeaef46938de6c78fd37497e81c0
DenisErmolovich <denisermolovich@fico.com>
2019-07-04 11:23:36 +0300

    progress

---
+++ by/oca/appendixb/Example.java
@@ -0,0 +1,12 @@
+package by.oca.appendixb;
+
+public class Example {
+	public static void main(String... args) {
+		int x = 0;
+		while(++x < 5) {
+			x += 1;
+		}
+		String message = x < 5 ? "Greater than" : "Less then";
+		System.out.println(message + "," + x);
+	}
+}
